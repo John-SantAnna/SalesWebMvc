038acae45aa4cf383f3831dfeb212b7000bf3660
John-SantAnna <john.santanna@outlook.com>
2020-12-26 23:20:57 -0300

    Teste 2

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -12,7 +12,7 @@ namespace SalesWebMvc {
     public class Program {
         public static void Main(string[] args) {
             CreateWebHostBuilder(args).Build().Run();
-        }  
+        }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
