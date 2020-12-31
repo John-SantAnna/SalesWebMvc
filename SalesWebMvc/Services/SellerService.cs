76c53738d35a64f6087801bc9097008d0b8d0f22
John-SantAnna <john.santanna@outlook.com>
2020-12-31 01:17:50 -0300

    SellerFormViewModel e componente select para departamento

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -22,8 +22,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller obj)
         {
-            obj.Department = _context.Department.First();
-            obj.Id = _context.Seller.Last().Id + 1;
+            obj.Id = _context.Seller.Last().Id + 1; // gambiarra, tem que arrumar!
             _context.Add(obj);
             _context.SaveChanges();
         }
